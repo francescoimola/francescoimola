ccba8c0927f2f426289e4b45f5c817cbd1485aaa
francescoimola <frn.imola@gmail.com>
2025-11-05 21:41:30 +0000

    Add Schema.org markup
    Introduced a new _schema.njk template to inject Schema.org structured data to all pages. Updated site.js to include a logo image for schema markup and included the schema template in _head.njk for improved SEO and rich search results.

--- src/_data/site.js
+++ src/_data/site.js
@@ -3,5 +3,6 @@ const pkg = require('../../package.json');
 module.exports = {
   url: pkg.url,
   name: pkg.name,
-  description: pkg.description
+  description: pkg.description,
+  image: `${pkg.url}/assets/favicon/android-chrome-512x512.png`, // Logo for schema markup
 };
